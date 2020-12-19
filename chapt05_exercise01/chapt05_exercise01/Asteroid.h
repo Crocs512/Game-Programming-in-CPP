81fc3fe0e0d71f6108614e637089d2e5963ed8d8
503470902@qq.com <503470902@qq.com>
2020-12-19 15:34:53 +0800

    chapter 05-exercise 01

---
+++ chapt05_exercise01/chapt05_exercise01/Asteroid.h
@@ -0,0 +1,21 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#pragma once
+#include "Actor.h"
+#include "CircleComponent.h"
+class Asteroid : public Actor
+{
+public:
+	Asteroid(class Game* game);
+	~Asteroid();
+	
+	class CircleComponent* GetCircle() { return mCircle; }
+private:
+	class CircleComponent* mCircle;
+};
